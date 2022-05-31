889ca360c81906368663c1cad12e4b73cf2b5e28
alabgh01 <alabgh01@luther.edu>
2022-05-31 18:55:59 -0500

    close menu on second click

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,12 @@
 /* jshint browser: true */
 /* jshint jquery: true */
 'use strict';
+
+function openCloseMenu(){
+    let menuContent = document.querySelector(".dropdown-content");
+    if (window.getComputedStyle(menuContent).display == "none"){
+        menuContent.style.display = "block";
+    }else if (window.getComputedStyle(menuContent).display == "block"){
+        menuContent.style.display = "none";
+    }
+}
