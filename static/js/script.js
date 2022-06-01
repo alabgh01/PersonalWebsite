a32b37b3fb6e5cd5fde2f9db81ade25302cb511d
alabgh01 <alabgh01@luther.edu>
2022-05-31 19:24:31 -0500

    cleaning and comments

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,7 @@
 /* jshint jquery: true */
 'use strict';
 
+// function for closing and openinig navbar on small screen on click
 function openCloseMenu(){
     let menuContent = document.querySelector(".dropdown-content");
     if (window.getComputedStyle(menuContent).display == "none"){
